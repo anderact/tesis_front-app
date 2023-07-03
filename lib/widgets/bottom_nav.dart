7008aec8781fc2265a1fb17f7796d2652519c451
anderact <axel.chacon@tecsup.edu.pe>
2023-07-03 17:48:38 -0500

    bottomnav final

--- lib/widgets/bottom_nav.dart
+++ lib/widgets/bottom_nav.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class BNavigator extends StatefulWidget {
   const BNavigator({super.key});
@@ -12,24 +13,24 @@ class _BNavigatorState extends State<BNavigator> {
   int index = 0;
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-        currentIndex: index,
-        onTap: (int i) {
-          setState(() {
-            index = i;
-          });
-        },
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.black,
-        unselectedItemColor: const Color.fromARGB(255, 179, 180, 184),
-        iconSize: 25.0,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Ionicons.home), label: 'Inicio'),
-          BottomNavigationBarItem(
-              icon: Icon(Ionicons.heart), label: 'Me gusta'),
-          BottomNavigationBarItem(icon: Icon(Ionicons.search), label: 'Buscar'),
-          BottomNavigationBarItem(icon: Icon(Ionicons.map), label: 'Mapa'),
-          BottomNavigationBarItem(icon: Icon(Ionicons.person), label: 'Yo')
-        ]);
+    return const Scaffold(
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 22, vertical: 20),
+        child: GNav(
+            hoverColor: Color.fromARGB(40, 158, 158, 158),
+            gap: 8,
+            activeColor: Colors.black,
+            color: Color.fromRGBO(178, 189, 183, 1),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+            duration: Duration(microseconds: 400),
+            tabs: [
+              GButton(icon: Ionicons.home),
+              GButton(icon: Ionicons.heart),
+              GButton(icon: Ionicons.search),
+              GButton(icon: Ionicons.map),
+              GButton(icon: Ionicons.person)
+            ]),
+      ),
+    );
   }
 }
